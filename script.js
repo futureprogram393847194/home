7e96222b43d516fb8b52b5adb3489eb0dab3e526
futureprogram393847194 <127174671+futureprogram393847194@users.noreply.github.com>
2023-07-03 16:12:34 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -18,3 +18,37 @@ document.getElementById("btn1").addEventListener("click", function() {
     window.location.href = "#"; // リンク先のURLを指定します
   });
   
+     //共通のオプション設定
+      ScrollReveal({
+        reset: true,
+        distance: "60px",
+        duration: 2000, //2sかけてアニメーション
+        delay: 200 /* 0.2s後に発動 */,
+      });
+
+      ScrollReveal().reveal(".main-title, .section-title", {
+        delay: 200,
+        origin: "left",
+      });
+      ScrollReveal().reveal(".sec-01 .image, .info", {
+        delay: 600,
+        origin: "bottom",
+      });
+      ScrollReveal().reveal(".text-box", {
+        delay: 700,
+        origin: "right",
+      });
+      ScrollReveal().reveal(".media-icons i", {
+        delay: 500,
+        origin: "bottom",
+        interval: 200,
+      });
+      ScrollReveal().reveal(".sec-02 .image, .sec-03 .image", {
+        delay: 500,
+        origin: "top",
+      });
+      ScrollReveal().reveal(".media-info li", {
+        delay: 500,
+        origin: "left",
+        interval: 200,
+      });
